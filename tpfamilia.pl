0749178a45b2c7236c272b3584aac50d26852890
ivogonzaa <49532329+ivogonzaa@users.noreply.github.com>
2019-05-28 15:15:03 -0300

    Add files via upload

---
+++ tpfamilia.pl
@@ -0,0 +1,41 @@
+padre(homero,lisa).
+padre(homero,bart).
+padre(homero,maggie).
+madre(marge,lisa).
+madre(marge,bart).
+madre(marge,maggie).
+
+madre(jacqueline,marge).
+madre(jacqueline,selma).
+madre(jacqueline,pati).
+
+padre(abraham,homero).
+
+hermano(A,B):-
+ hijo(A,X),
+ hijo(B,X),
+ A \= B.
+ 
+hijo(A,X):-
+ padre(X,A).
+
+hijo(A,X):-
+ madre(X,A).
+ 
+sobrino(A,Z):-
+ hijo(A,X),
+ hermano(Z,X).
+ 
+tio(Z,A):-
+ sobrino(A,Z).
+ 
+abuelo(Y,A):-
+ hijo(A,X),
+ hijo(X,Y).
+ 
+primo(A,C):-
+ tio(Z,A),
+ tio(X,C),
+ hijo(A,X),
+ hijo(C,Z).
+
